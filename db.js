fb16bd499187c8d8a0861d4a3ac85b8c3463d4b6
nihar021 <jenanihar593@gmail.com>
2025-01-04 20:36:28 +0530

    Your descriptive commit message here

--- db.js
+++ db.js
@@ -1,21 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// Define the MongoDB connection URL
-const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
-// const mongoURL = process.env.MONGODB_URL;
 
-// Set up MongoDB connection
+const mongoURL = process.env.MONGODB_URL_LOCAL 
+
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
-// Get the default connection
-// Mongoose maintains a default connection object representing the MongoDB connection.
 const db = mongoose.connection;
 
-// Define event listeners for database connection
 
 db.on('connected', () => {
     console.log('Connected to MongoDB server');
@@ -29,6 +24,5 @@ db.on('disconnected', () => {
     console.log('MongoDB disconnected');
 });
 
-// Export the database connection
 module.exports = db;
 
